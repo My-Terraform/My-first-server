8c995574bc64296d6221f77cc60dcdc6f2c4b54f
vipulsonar26 <vipul_sonar@persistent.co.in>
2021-03-26 19:54:27 +0530

    added tag name

--- Deploy_instance.tf
+++ Deploy_instance.tf
@@ -1,11 +1,14 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQRX36KXLNSH7S3M5"
-  secret_key = "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"
+  access_key = "AKIAQRX36KXLD6Z37OP"
+  secret_key = "vAQAs8NHwvBYVN2wvUIy0xA1JoCW7olEiw79"
 }
 
 resource "aws_instance" "my-first-server" {
   ami = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
+  tags = {
+    "name" = "ubuntu"
+  }
 }
 
